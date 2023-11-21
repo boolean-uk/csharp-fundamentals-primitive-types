ceeb0d4f973a203b4caeea36b0f252d86580438c
Nikita-vB <n.vanberendonk@gmail.com>
2023-11-21 13:38:48 +0100

    Exercise complete (static members fail the test (7, 8, 9, 10, 12, 13, 14), but succeed when not static.

--- src/csharp-fundamentals-primitive-types.Main/Program.cs
+++ src/csharp-fundamentals-primitive-types.Main/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using csharp_fundamentals_primitive_types.Main;
+
 Console.WriteLine("Hello, World!");
+
+string firstName = "Jane";
+string lastName = "Smith";
+string fullName = firstName + " " + lastName;
+Console.WriteLine(fullName);
+
+string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char tenthLetter = alphabet.ToCharArray().ElementAtOrDefault(9);
+Console.WriteLine(tenthLetter);
+
+string lowerAlphabet = Core.alphabet.ToLower();
+Console.WriteLine(lowerAlphabet);
