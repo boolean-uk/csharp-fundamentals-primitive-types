ccd27733510a5491cdd027ec641657d581a8a70c
krzysztofmmm <morydzkrzysztof@gmail.com>
2024-01-09 11:06:28 +0100

    Primitive types Task

---
+++ csharp-fundamentals-primitive-types.Main/Extensions/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_fundamentals_primitive_types.Extensions
+{
+    internal class Movie
+    {
+        public string? title = "2012";
+        public string? type = "disaster movie";
+        public DateOnly? year = new DateOnly(2012 , 1 , 1);
+        public List<string> actors = new List<string> { "Tom Hanks" , "Meryl Streep" , "Denzel Washington" , "Viola Davis" , "Leonardo DiCaprio" };
+    }
+}
---
+++ csharp-fundamentals-primitive-types.Main/Extensions/SmPost.cs
@@ -0,0 +1,11 @@
+﻿namespace csharp_fundamentals_primitive_types.Extensions
+{
+    internal class SmPost
+    {
+        public string user = "some user";
+        public string content = "some content of post";
+        public int views = 1241;
+        public int likes = 12;
+        public int comments = 20;
+    }
+}
