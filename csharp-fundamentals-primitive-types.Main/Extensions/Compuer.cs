cbbea8ce82191276122f595fde9710b65f289666
André Sturesson <git@asturesson.se>
2024-01-09 09:35:53 +0100

    Fixed syntax error

--- csharp-fundamentals-primitive-types.Main/Extensions/Compuer.cs
+++ csharp-fundamentals-primitive-types.Main/Extensions/Compuer.cs
@@ -2,11 +2,11 @@
 
 public class Computer {
     private string processor {get; set;}
-    private int speed; {get; set;}
+    private int speed {get; set;}
 
     private int ram {get; set;}
 
-    private int storage {get; set}
+    private int storage {get; set;}
 
     public Computer(string processor, int speed, int ram, int storage) {
         this.processor = processor;
