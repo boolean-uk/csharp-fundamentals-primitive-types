c2dd5470fb8dea0eb8de6255a5568a89419b11d4
llllllll-l <vincent.werelius@gmail.com>
2024-01-09 09:09:10 +0100

    test push

--- csharp-fundamentals-primitive-types.Main/Core.cs
+++ csharp-fundamentals-primitive-types.Main/Core.cs
@@ -12,7 +12,7 @@ namespace csharp_fundamentals_primitive_types.Main
 
 
         // 1. Change the value of the member below to be the result of adding numOne and numTwo together
-        public int numOnePlusTwo = 0;
+        public int numOnePlusTwo = numOne + numTwo;
 
 
         // 2. Change the value of the member below to be the result of multiplying numThree by numTwo
