bab5eab1a1fbd2bd14626b9fce84b09981d0edd9
Ali Haider Khan <ali_786@live.no>
2024-08-12 14:50:03 +0200

    Gjort ferdig oppgavene i del 1.

--- csharp-fundamentals-primitive-types.Main/Core.cs
+++ csharp-fundamentals-primitive-types.Main/Core.cs
@@ -11,7 +11,7 @@ namespace csharp_fundamentals_primitive_types.Main
         public static int numThree = 32;
 
         // 1. Change the value of the member below to be the result of adding numOne and numTwo together
-        public int numOnePlusTwo = numOne + numOne;
+        public int numOnePlusTwo = numOne + numTwo;
 
         // 2. Change the value of the member below to be the result of multiplying numThree by numTwo
         public int numThreeTimesNumTwo = numThree * numTwo;
@@ -26,36 +26,37 @@ namespace csharp_fundamentals_primitive_types.Main
         public int sum = numOne + numTwo + numThree;
 
         // 6. Change the value of the member below to be the sum of numOne, numTwo and numThree divided by numOne
-        public int numBytes = 0;
+        public int numBytes = (numOne + numTwo + numThree) / numOne;
 
         // 7. Create a public char member named lastLetter containing the last letter of the English alphabet
         public char lastLetter = 'Z';
 
         // 8. Create a public float member named pi that contains the value of pi to two decimal places
-        public float pi = 0f;
+        public float pi = 3.14f;
 
         // 9. Create a public double member named piD that contains the value of pi to 5 decimal places
-        public double piD = 0;
+        public double piD = 3.14159;
 
         public static string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         public static string firstName = "Jane";
         public static string lastName = "Smith";
 
         // 10. Create a public member named fullName that contains the value of firstName and lastName concatenated together with a space in between
-        public string fullName = "";
+        public string fullName = firstName + " " + lastName;
 
         // 11. Fix the line below so that tenthLetter contains the tenth letter in the alphabet member above.
-        public char tenthLetter = alphabet.ToCharArray().ElementAtOrDefault(0);
+        public char tenthLetter = alphabet.ToCharArray().ElementAtOrDefault(9);
 
         // 12. Create a public string member named lowerAlphabet that contains the value of the alphabet member in all lower case characters
         // If you need help, look through the available String methods to find a relevant one here: https://www.w3schools.com/cs/cs_strings.php
-        public string lowerAlphabet = "";
+        public string lowerAlphabet = alphabet.ToLower();
+
 
         // 13. Create a public integer member named alphabetLength that contains the number of characters that exist in the alphabet member
         // Use the documentation linked above if you need help
-        public int alphabetLength = 0;
+        public int alphabetLength = 26;
 
         // 14. Create a public integer member named remainder that contains the remainder of dividing 15 by 8
-        public int remainder = 0;
+        public int remainder = 15 % 8;
     }
 }
