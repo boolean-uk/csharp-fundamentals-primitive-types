633a2566c4a4e9a9a92d5de4c7a85d47dd00b0b3
Anders Hagen Ottersland <anderh0709@gmail.com>
2024-08-12 14:45:24 +0200

    Done all the core tasks and created a car class

---
+++ csharp-fundamentals-primitive-types.Main/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace csharp_fundamentals_primitive_types
+{
+    public class Car
+    {
+        public String Make;
+        public String model;
+        public String transmission_type;
+        public int number_of_doors;
+        public float engine_litres;
+        public String engine_type;
+        public String colour;
+        public String registration;
+
+        //- Make, model, transmission type, number of doors, engine litres, engine type, colour, registration
+    }
+}
