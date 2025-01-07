73f981fd5b31932637f69d9d17213fef2aa33df2
Jone Hjorteland <jone.hjorteland@experisacademy.no>
2025-01-07 14:38:26 +0100

    Finished extensions

--- csharp-fundamentals-primitive-types.Main/Core.cs
+++ csharp-fundamentals-primitive-types.Main/Core.cs
@@ -58,4 +58,117 @@ namespace csharp_fundamentals_primitive_types.Main
         // 14. Create a public integer member named remainder that contains the remainder of dividing 15 by 8
         public int remainder = 15 - (8 * (15 / 8));
     }
+
+    public class Book
+    {
+        public String title = "A Game of Thrones";
+        public int publishYear = 1996;
+        public float price = 11.99f;
+        public String isbn = "0-00-224584-1";
+        public String author = "George R. R. Martin";
+    }
+
+    public class Car
+    {
+        public required string Make { get; set; }
+        public required string Model { get; set; }
+        public string? TransmissionType { get; set; }
+        public int? NumberOfDoors { get; set; }
+        public int? EngineLiters { get; set; }
+        public string? EngineType { get; set; }
+        public string? Colour { get; set; }
+        public string? Registration { get; set; }
+    }
+
+    public class Movie
+    {
+        public required string Title { get; set; }
+        public required int ReleaseYear { get; set; }
+        public string? ReleaseDate { get; set; }
+        public string? Description { get; set; }
+        public List<string>? Genres { get; set; }
+        public string? ImdbUrl { get; set; }
+    }
+
+    public class Computer
+    {
+        public required string Case { get; set; }
+        public required string Motherboard { get; set; }
+        public required string Cpu { get; set; }
+        public required string Ram { get; set; }
+        public required string PowerSupply { get; set; }
+        public required List<string> Disks { get; set; }
+        public string? Gpu { get; set; }
+    }
+
+    public class SocialMediaPost
+    {
+        public required string PostId { get; set; }
+        public required string PosterId { get; set; }
+        public required string PostDate { get; set; }
+        public required string PostTitle { get; set; }
+
+        public string? PostDescription { get; set; }
+        public string? MediaUrl { get; set; }
+
+        public int? Likes { get; set; }
+        public int? Dislikes { get; set; }
+    }
+
+    public class User
+    {
+        public required string Uuid { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required DateOnly DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Country { get; set; }
+        public string? Address { get; set; }
+    }
+
+    public class ECommerceProduct
+    {
+        public required string Sku { get; set; }
+        public required string Name { get; set; }
+        public required float Price { get; set; }
+        public required int Stock { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class ECommerceOrder
+    {
+        public required string OrderId { get; set; }
+        public required string CustomerId { get; set; }
+        public required DateTime OrderTime { get; set; }
+        public required string Sku { get; set; }
+        public required int Amount { get; set; }
+        public required float Price { get; set; }
+        public required string DeliveryCountry { get; set; }
+        public required string DeliveryAddress { get; set; }
+    }
+
+    public class TV
+    {
+        public required string Name { get; set; }
+        public required string Manufacturer { get; set; }
+        public required string PanelTechnology { get; set; }
+        public required int ResolutionHeight { get; set; }
+        public required float ResolutionWidth { get; set; }
+        public required float Refreshrate { get; set; }
+        public required float Size { get; set; }
+        public required float Weight { get; set; }
+    }
+
+    public class CPU
+    {
+        public required string Name { get; set; }
+        public required string Model { get; set; }
+        public required string Manufacturer { get; set; }
+        public required int Cores { get; set; }
+        public required int Threads { get; set; }
+        public required float ClockSpeed { get; set; }
+        public required int Tdp { get; set; }
+    }
 }
